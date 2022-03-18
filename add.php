9723ccf0d590856231702a9df1dac4d7dee9ade7
Siiri Poropudas <n1posi00@students.oamk.fi>
2022-03-18 17:25:49 +0200

    Coding

---
+++ add.php
@@ -0,0 +1,33 @@
+<?php
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Max-Age: 3600');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    return 0;
+}
+
+$input = json_decode(file_get_contents('php://input'));
+$description = filter_var($input->description,FILTER_SANITIZE_SPECIAL_CHARS);
+$amount = filter_var($input->description,FILTER_SANITIZE_NUMBER_INT);
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $query = $db->prepare('insert into item(description,amount) values (:description,:amount)');
+    $query->bindValue(':description',$description,PDO::PARAM_STR);
+    $query->bindValue(':amount',$amount,PDO::PARAM_STR);
+    $query->execute();
+
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $db->lastInsertId(),'description' => $description, 'amount' => $amount);
+    print json_encode($data);
+} catch (PDOException $pdoex) {
+    header('HTTP/1.1 500 Internal Server Error');
+    $error = array('error' => $pdoex->getMessage());
+    print json_encode($error);
+}
